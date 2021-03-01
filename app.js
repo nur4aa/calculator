5142e482dcca481545277529d979f5772223a7c4
nur4aa <38027647+nur4aa@users.noreply.github.com>
2021-03-01 11:08:46 +0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+let input = document.querySelector('.input')
+
+function insert(num) {
+    input.textContent = input.textContent + num;
+}
+
+function clean() {
+    input.textContent= ''
+}
+
+function back() {
+    let exp = input.textContent
+    input.textContent = exp.substring(0,exp.length-1)
+}
+
+function equal() {
+    const exp = input.textContent;
+    if (exp) {
+        input.textContent = eval(exp)
+    }
+}
